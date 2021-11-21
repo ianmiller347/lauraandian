f82a74e2c0d1e72646ddc1574e9b00ea96608253
Ian Miller 🦐 <ianmiller347@gmail.com>
2021-11-21 12:53:59 -0500

    fix fonts, edit home page details section

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -12,37 +12,34 @@ const Home = () => {
       <div className="callout-content">
         <h3 className="section-title">Welcome Party</h3>
         <div className="date">Friday, September 23, 2022</div>
-        <div className="flex">
+        <div className="time-and-details">
           <div className="time">
-            <h4>7:30 PM</h4>
+            <h4>7:30 PM: Welcome party</h4>
           </div>
-          <div className="">
-            <h4>Welcome party</h4>
+          <div className="details">
             <div>Location, location</div>
             <div>Attire: Cocktail</div>
           </div>
         </div>
         <h3 className="section-title">Wedding Day</h3>
-        <div className="">September 24, 2022</div>
-        <div className="flex">
+        <div className="date">September 24, 2022</div>
+        <div className="time-and-details">
           <div className="time">
-            <h4>5:30 PM</h4>
+            <h4>5:30 PM: Ceremony</h4>
           </div>
-          <div>
-            <h4>Ceremony</h4>
+          <div className="details">
             <div>Immaculate Conception Jesuit Church</div>
             <div>130 Baronne Street, New Orleans, LA, 70112</div>
             <div>Attire: Cocktail</div>
           </div>
         </div>
-        <div className="flex">
+        <div className="time-and-details">
           <div className="time">
-            <h4>7:00 PM</h4>
+            <h4>7:00 PM: Reception</h4>
           </div>
-          <div>
-            <h4>Reception</h4>
+          <div className="details">
             <div>Orléans Club</div>
-            <div>5005 Saint Charles Avenue, New Orleans, LA, 70115, USA</div>
+            <div>5005 Saint Charles Avenue, New Orleans, LA, 70115</div>
             <div>Attire: Cocktail</div>
           </div>
         </div>
